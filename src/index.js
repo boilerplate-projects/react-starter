60b3e642bdb589e456d205ec96c4adbfbdcb34f3
Daniel Beff <daniel.beff@gmail.com>
2017-06-29 23:42:20 +0200

    Added support for scss extension

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './scripts/App';
-import registerServiceWorker from './js/registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./scripts/components/App";
+import registerServiceWorker from "./scripts/registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
